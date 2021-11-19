2876a8156b539bae85c5f4f3a55fd1de60359fd8
kefanyu <59634672+kefanyu@users.noreply.github.com>
2021-11-19 11:06:01 -0800

    Initial Commit

---
+++ GauchoDivination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GauchoDivination
+//
+//  Created by 凤凰院红脸 on 11/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
