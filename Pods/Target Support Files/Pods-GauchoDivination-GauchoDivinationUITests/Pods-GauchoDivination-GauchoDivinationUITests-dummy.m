2b695336a5f1fb542844c4a6891af39d39384883
kefanyu <59634672+kefanyu@users.noreply.github.com>
2021-11-19 22:48:44 -0800

    new file

---
+++ Pods/Target Support Files/Pods-GauchoDivination-GauchoDivinationUITests/Pods-GauchoDivination-GauchoDivinationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GauchoDivination_GauchoDivinationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GauchoDivination_GauchoDivinationUITests
+@end
---
+++ Pods/Target Support Files/Pods-GauchoDivinationTests/Pods-GauchoDivinationTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GauchoDivinationTests : NSObject
+@end
+@implementation PodsDummy_Pods_GauchoDivinationTests
+@end
